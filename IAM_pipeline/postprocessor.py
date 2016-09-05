580ac9b3191d97cf895d3b6f097cccdc2d61cb9d
englaenda <timothy.smith@cdtm.de>
2016-09-05 16:03:53 +0200

    IAM_pipeline/postprocessor.py

--- IAM_pipeline/postprocessor.py
+++ IAM_pipeline/postprocessor.py
@@ -8,22 +8,25 @@ class IAM_Postprocessor(PostprocessorTask):
 
         print("True: ", input_tuple[0])
         print("Pred: ", input_tuple[1].shape)
-        print("Cost: ", input_tuple[2].shape)
+        print("Cost: ", input_tuple[2])
         print("Aux: ", input_tuple[3].shape)
 
         chars = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3',
                  '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
                  'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[',
                  '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-                 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']  # data['chars']
+                 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
         printer = utils.Printer(chars)
 
         print('Shown : ', end='')
-        printer.yprint(input_tuple[0])
+        labels_out_true, labels_list_true = printer.yprint(input_tuple[0])
 
         print('Seen  : ', end='')
         maxes = np.argmax(input_tuple[1], 0)
-        printer.yprint(maxes)
+        labels_out_pred, labels_list_pred = printer.yprint(maxes)
+
+        print('True String: ', labels_out_true)
+        print('Pred String: ', labels_out_pred)
 
         return [np.random.random((6, 6))]
 

d26e1e8a209126d945b8a1efcb94704b1155d99e
englaenda <timothy.smith@cdtm.de>
2016-09-16 17:12:11 +0200

    jetzt gehts

--- IAM_pipeline/predictor.py
+++ IAM_pipeline/predictor.py
@@ -169,8 +169,8 @@ class IAM_Predictor(PredictorTask):
         # print(inputs[0])
         # print(inputs[1])
 
-        self.model.train_on_batch(self, x=inputs[0], y=inputs[1], class_weight=None, sample_weight=None)  #TODO metrics?
-
+        # self.model.train_on_batch(self, x=inputs[0], y=inputs[1], class_weight=None, sample_weight=None)  #TODO metrics?
+        self.model.fit(inputs[0], inputs[1], batch_size=1, nb_epoch=1)
         loss = inputs[1]
 
         return loss

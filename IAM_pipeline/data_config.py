0a35bcde7fc2e39bb4022e0ccb8f9236daa79536
englaenda <timothy.smith@cdtm.de>
2016-10-05 10:39:58 +0200

    debug wer

--- IAM_pipeline/data_config.py
+++ IAM_pipeline/data_config.py
@@ -10,6 +10,7 @@ outfile = 'IAM_output_NN.txt'
 # ___________________________
 # ___________ IAM ___________
 # ___________________________
+print("Loading IAM-Dataset")
 
 IAM_BASE_PATH = "../media/nas/01_Datasets/IAM/"
 IAM_img_path = IAM_BASE_PATH + "lines/"
--- IAM_pipeline/predictor.py
+++ IAM_pipeline/predictor.py
@@ -172,6 +172,7 @@ class TimCallback(keras.callbacks.Callback):
             self.word_error_rate = 0
         else:
             self.word_error_rate = 1
+        import ipdb;ipdb.set_trace()
         self.WER = wer(decoded_res, self.true_string)
 
     def on_epoch_end(self, epoch, logs={}):
